8460d8223de0b3c56f98a4c1bd00890f022acd5b
MikitaCat <stankevich.mikita21@gmail.com>
2022-06-22 11:34:50 +0200

    First components for Board and Cell have been created

---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CellComponent = () => {
+  return (
+    <div className="cell">
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default CellComponent;
