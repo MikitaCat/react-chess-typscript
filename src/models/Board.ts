2f4acd3370eee67c0349233a360abfbb13363512
MikitaCat <stankevich.mikita21@gmail.com>
2022-06-22 12:04:22 +0200

    Classes for Board and Cells have been created

---
+++ src/models/Board.ts
@@ -0,0 +1,21 @@
+import { Figure } from "./figures/Figure";
+import { Colors } from "./Colors";
+import { Cell } from "./Cell";
+
+export class Board {
+  cells: Cell[][] = [];
+
+  public initCells() {
+    for (let i = 0; i < 8; i++) {
+      const row: Cell[] = [];
+      for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 !== 0) {
+          row.push(new Cell(this, j, i, Colors.BLACK, null)); //Black Cells
+        } else {
+          row.push(new Cell(this, j, i, Colors.WHITE, null)); //White Cells
+        }
+      }
+      this.cells.push(row);
+    }
+  }
+}
