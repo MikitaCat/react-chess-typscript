f5541706eb8e73b68cdb8a03b059dd49ae52b760
MikitaCat <stankevich.mikita21@gmail.com>
2022-06-23 08:48:27 +0200

    Emplemented the figure moving

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -12,6 +12,14 @@ const BoardComponent: React.FC<BoardProps> = ({ board, setBoard }) => {
   const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
 
   function click(cell: Cell) {
+    if (
+      selectedCell &&
+      selectedCell !== cell &&
+      selectedCell.figure?.canMove(cell)
+    ) {
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+    }
     if (cell.figure) {
       setSelectedCell(cell);
     }
