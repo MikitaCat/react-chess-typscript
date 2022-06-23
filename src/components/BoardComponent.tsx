03caef5f3fd52e130f6bd3759359fb6937d382ae
MikitaCat <stankevich.mikita21@gmail.com>
2022-06-23 06:35:48 +0200

    Creqted a possibillity to choise the figure

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,19 +1,38 @@
 import React from "react";
 import { Board } from "../models/Board";
 import CellComponent from "./CellComponent";
+import { useState } from "react";
+import { Cell } from "../models/Cell";
 
 interface BoardProps {
   board: Board;
   setBoard: (board: Board) => void;
 }
 const BoardComponent: React.FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell: Cell) {
+    if (cell.figure) {
+      setSelectedCell(cell);
+    }
+  }
+
   return (
     <div className="board">
       {board.cells.map((row, index) => {
         return (
           <React.Fragment key={index}>
             {row.map((cell) => {
-              return <CellComponent cell={cell} key={cell.id} />;
+              return (
+                <CellComponent
+                  cell={cell}
+                  key={cell.id}
+                  selected={
+                    cell.x === selectedCell?.x && cell.y === selectedCell?.y
+                  }
+                  click={click}
+                />
+              );
             })}
           </React.Fragment>
         );
